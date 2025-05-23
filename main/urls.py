62e669606b0f23ac4df48ef20071439ace2b0cdf
AZDemonEdge <rene001208@gmail.com>
2025-05-23 17:57:14 -0400

    Add comments to Rutes

--- main/urls.py
+++ main/urls.py
@@ -5,25 +5,25 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.view_ppal, name='main'),
-    path('directorio/', views.view_directorio, name='directorio'),
-    path('user/', views.view_userpage, name='user'),
-    path('user/edit/', views.update_user, name='update_user'),
-    path('register/', views.register, name='register'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
+    path('', views.view_ppal, name='main'), # Home
+    path('directorio/', views.view_directorio, name='directorio'), # Directorio
+    path('user/', views.view_userpage, name='user'), # Perfil de Usuario
+    path('user/edit/', views.update_user, name='update_user'), # Actualizar Perfil
+    path('register/', views.register, name='register'), # Registrarse
+    path('login/', views.login_view, name='login'), # Logearse
+    path('logout/', views.logout_view, name='logout'), # Desloguearse
     # RUTAS DE CITAS
-    path('appointments/', views.appointment_index, name='appointment_index'),
-    path('appointments/create/', views.appointment_create, name='appointment_create'),
-    path('appointments/<int:id>', views.appointment_get, name='appointment_get'),
-    path('appointments/update/<int:id>', views.appointment_update, name='appointment_update'),
-    path('appointments/<int:id>/delete/', views.appointment_delete, name='appointment_delete'),
+    path('appointments/', views.appointment_index, name='appointment_index'), # Ver Listado de Citas
+    path('appointments/create/', views.appointment_create, name='appointment_create'), # Realizar Solicitud
+    path('appointments/<int:id>', views.appointment_get, name='appointment_get'), # Ver Solicitud
+    path('appointments/update/<int:id>', views.appointment_update, name='appointment_update'), # Actualizar Solicitud
+    path('appointments/<int:id>/delete/', views.appointment_delete, name='appointment_delete'), # Eliminar Solicitud
     # RUTAS DE RECURSOS
-    path('resources/', views.resource_index, name='resource_index'),
-    path('resources/create/', views.resource_update, name='resource_update'),
-    path('resources/<int:id>', views.resource_get, name='resource_get'),
-    path('resources/update/<int:id>', views.resource_update, name='resource_update'),
-    path('resources/<int:id>/delete/', views.resource_delete, name='resource_delete'),
+    path('resources/', views.resource_index, name='resource_index'), # Ver Listao de Recursos
+    path('resources/create/', views.resource_load, name='resource_load'), # Subir recurso
+    path('resources/<int:id>', views.resource_get, name='resource_get'), # Obtener Recurso
+    path('resources/update/<int:id>', views.resource_update, name='resource_update'), # Editar Recurso
+    path('resources/<int:id>/delete/', views.resource_delete, name='resource_delete'), # Eliminar Recurso
 ]
 
 # Configuración para servir archivos multimedia en desarrollo
